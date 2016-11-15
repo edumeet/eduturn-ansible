a371f750b889ae6dac68ef1b9cea8192c9715d4a
Mészáros Mihály <misi@niif.hu>
2016-11-15 09:45:22 +0100

    add psnc hosts

---
+++ host_vars/turn1.man.poznan.pl
@@ -0,0 +1,25 @@
+dns:
+ search_domain: man.poznan.pl
+ local_domain: man.poznan.pl
+ ipv6_domain_name_server1: 2620:0:ccc::2
+ ipv6_domain_name_server2: 2620:0:ccd::2
+ ipv4_domain_name_server1: 150.254.173.2
+ ipv4_domain_name_server2: 150.254.173.3
+
+timezone: Europe/Warsaw
+
+ipv4:
+ address: 150.254.161.36
+ netmask: 255.255.255.224
+ gateway: 150.254.161.32
+ipv6:
+ address: 2001:808:2:3005::161:36
+ netmask: 64
+ gateway: 2001:808:2:305::1
+
+host:
+ hostname: turn1
+ domain: man.poznan.pl
+
+# mysql
+mysql_server_id: 11
