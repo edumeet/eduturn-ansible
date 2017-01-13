cea38fd09207ec384a87aede863994349c61be5b
Mészáros Mihály <misi@niif.hu>
2017-01-13 11:07:07 +0100

    add primary key id

--- roles/coturn/files/coturn-rest_schema.sql
+++ roles/coturn/files/coturn-rest_schema.sql
@@ -1,56 +1,73 @@
 
 CREATE TABLE turnusers_lt (
-    realm varchar(127) default '',
-    name varchar(512),
-    hmackey char(128),
-    PRIMARY KEY (realm,name)
+        id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+        realm varchar(127) default '',
+        name varchar(512),
+        hmackey char(128),
+        primary key (id),
+        key (realm,name)
 );
 
 CREATE TABLE turn_secret (
+        id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 	realm varchar(127) default '',
 	value varchar(256),
-	primary key (realm,value)
+        timestamp timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        primary key (id),
+	key (realm,value)
 );
 
 CREATE TABLE allowed_peer_ip (
+        id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 	realm varchar(127) default '',
 	ip_range varchar(256),
-	primary key (realm,ip_range)
+        primary key (id),
+	key (realm,ip_range)
 );
 
 CREATE TABLE denied_peer_ip (
+        id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 	realm varchar(127) default '',
 	ip_range varchar(256),
-	primary key (realm,ip_range)
+        primary key (id),
+	key (realm,ip_range)
 );
 
 CREATE TABLE turn_origin_to_realm (
+        id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 	origin varchar(127),
 	realm varchar(127),
-	primary key (origin)
+        primary key (id),
+	key (origin)
 );
 
 CREATE TABLE turn_realm_option (
+        id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 	realm varchar(127) default '',
 	opt varchar(32),
 	value varchar(128),
-	primary key (realm,opt)
+        primary key (id),
+	key (realm,opt)
 );
 
 CREATE TABLE oauth_key (
+        id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 	kid varchar(128),
 	ikm_key varchar(256),
 	timestamp bigint default 0,
 	lifetime integer default 0,
 	as_rs_alg varchar(64) default '',
 	realm varchar(127),
-	primary key (kid)
+        primary key (id),
+	key (kid)
 );
 
 CREATE TABLE admin_user (
+        id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
 	name varchar(32),
 	realm varchar(127),
 	password varchar(127),
-	primary key (name)
+        primary key (id),
+	key (name)
 );
 
