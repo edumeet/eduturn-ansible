23541cf65ac5b725a23e4dba7d28345dd5ee8989
Mészáros Mihály <misi@niif.hu>
2017-01-13 11:30:10 +0100

    add inventory mysql schema

---
+++ roles/coturn/files/inventory_schema.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `server` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `fqdn` varchar(255) DEFAULT NULL,
+  `organization` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `ip` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `server_id` bigint(20) unsigned NOT NULL,
+  `ip` varchar(45) NOT NULL COMMENT 'IPv4/IPv6',
+  `ipv6` tinyint(1) NOT NULL,
+  `latitude` double DEFAULT NULL,
+  `longitude` double DEFAULT NULL,
+  `preference` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `ip` (`ip`),
+  KEY `server_id` (`server_id`),
+  CONSTRAINT `ip_ibfk_1` FOREIGN KEY (`server_id`) REFERENCES `server` (`id`)
+);
+
+CREATE TABLE `service` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `ip_id` bigint(20) unsigned DEFAULT NULL,
+  `port` smallint(5) unsigned DEFAULT '3478',
+  `protocol` enum('udp','tcp','sctp') DEFAULT NULL,
+  `uri_schema` enum('stun','stuns','turn','turns') DEFAULT NULL,
+  `preference` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ip_id` (`ip_id`),
+  CONSTRAINT `service_ibfk_1` FOREIGN KEY (`ip_id`) REFERENCES `ip` (`id`)
+);
+
