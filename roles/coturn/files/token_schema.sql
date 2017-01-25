863d6d36f922c9b26e1fe826b0c4fd475b5dc1bd
Dariusz Janny <janny@man.poznan.pl>
2017-01-25 14:42:57 +0100

    Added DROP TABLE IF EXISTS in MySQL scripts

--- roles/coturn/files/token_schema.sql
+++ roles/coturn/files/token_schema.sql
@@ -1,13 +1,14 @@
+DROP TABLE IF EXISTS `token`;
 CREATE TABLE `token` (
-  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `eppn` varchar(128) DEFAULT NULL,
-  `email` varchar(128) DEFAULT NULL,
-  `displayname` varchar(60) DEFAULT NULL,
-  `token` varchar(64) NOT NULL,
-  `service_url` varchar(500) DEFAULT NULL,
-  `realm` varchar(127) DEFAULT NULL,
-  `created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  KEY `eppn` (`eppn`),
-  KEY `email` (`email`)
+	`id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+	`eppn` varchar(128) DEFAULT NULL,
+	`email` varchar(128) DEFAULT NULL,
+	`displayname` varchar(60) DEFAULT NULL,
+	`token` varchar(64) NOT NULL,
+	`service_url` varchar(500) DEFAULT NULL,
+	`realm` varchar(127) DEFAULT NULL,
+	`created` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	KEY `eppn` (`eppn`),
+	KEY `email` (`email`)
 ) DEFAULT CHARSET=utf8;
